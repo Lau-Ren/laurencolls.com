0a3216ffd944d881cbe09edbb8f913ea3b1146ea
lau-ren <colls.lauren@gmail.com>
2016-07-03 21:11:25 +1200

    adjust colours

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
 
-render(<App name='laurencolls.com' />, document.querySelector('main'))
+render(<App />, document.querySelector('main'))
 console.log('welcome to laurencolls.com')
